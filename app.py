a5b71d0b7d8b39f870dadafa7e9975279cb564ac
Shilpa Shaw <135813624+ShilpaShaw17@users.noreply.github.com>
2024-06-16 18:21:06 +0530

    app.py

--- app.py
+++ app.py
@@ -57,34 +57,34 @@ submit1 = st.button("Tell Me About the Resume")
 submit3 = st.button("Percentage match")
 
 input_prompt1 = """
- You are an experienced Technical Human Resource Manager,your task is to review the provided resume against the job description. 
-  Please share your professional evaluation on whether the candidate's profile aligns with the role. 
- Highlight the strengths and weaknesses of the applicant in relation to the specified job requirements.
+Imagine you are a technical recruiter tasked with evaluating a candidate's resume for a technical role. 
+Your objective is to provide a comprehensive assessment of how well the applicant's skills and experience match the job requirements.
+Highlight areas where the candidate excels and areas that may need improvement in the specified role.
 """
 
 input_prompt3 = """
-You are an skilled ATS (Applicant Tracking System) scanner with a deep understanding of data science and ATS functionality, 
+You are a skilled ATS (Applicant Tracking System) scanner with a deep understanding of data science and ATS functionality, 
 your task is to evaluate the resume against the provided job description. give me the percentage of match if the resume matches
-the job description. First the output should come as percentage and then keywords missing and last final thoughts.
+the job description. First, the output should come as a percentage and then keywords missing and last final thoughts.
 """
 
 if submit1:
     if uploaded_file is not None:
         pdf_content=input_pdf_setup(uploaded_file)
         response=get_gemini_response(input_prompt1,pdf_content,input_text)
-        st.subheader("The Repsonse is")
+        st.subheader("The Response is")
         st.write(response)
     else:
-        st.write("Please uplaod the resume")
+        st.write("Please upload the resume")
 
 elif submit3:
     if uploaded_file is not None:
         pdf_content=input_pdf_setup(uploaded_file)
         response=get_gemini_response(input_prompt3,pdf_content,input_text)
-        st.subheader("The Repsonse is")
+        st.subheader("The Response is")
         st.write(response)
     else:
-        st.write("Please uplaod the resume")
+        st.write("Please upload the resume")
 
 
 
